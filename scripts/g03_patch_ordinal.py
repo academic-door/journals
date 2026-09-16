9b3ea095ea822099cc549110878e6e1022cbc072
Simon <252317962+SIMON-WORLD@users.noreply.github.com>
2026-09-16 13:10:21 -0400

    fix: preserve regex escapes in ordinal patch executor

--- scripts/g03_patch_ordinal.py
+++ scripts/g03_patch_ordinal.py
@@ -7,7 +7,7 @@ if marker in text:
     raise SystemExit(0)
 
 insertion_point = text.index("_EN_NUMBER_UNIT_RE = re.compile(")
-block = '''_EN_ORDINAL_BASE_VALUES = {
+block = r'''_EN_ORDINAL_BASE_VALUES = {
     "first": 1, "second": 2, "third": 3, "fourth": 4, "fifth": 5,
     "sixth": 6, "seventh": 7, "eighth": 8, "ninth": 9, "tenth": 10,
     "eleventh": 11, "twelfth": 12, "thirteenth": 13, "fourteenth": 14,
